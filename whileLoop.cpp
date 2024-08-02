f00032c3783f00f3bcc6c4e0e24907e23bf63161
jcasanova1990 <jeremykcasanova@gmail.com>
2024-08-02 11:55:23 -0400

    third

---
+++ whileLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int count = 5;
+    while (count > 0) {
+        cout << "This is Whiled. LOL" << endl;
+        count--;
+    }
+
+    return 0;
+}
