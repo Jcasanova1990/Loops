d55b9442755c896f3d2cae024a31c8f945f1af65
jcasanova1990 <jeremykcasanova@gmail.com>
2024-08-02 12:00:02 -0400

    fourth

--- whileLoop.cpp
+++ whileLoop.cpp
@@ -9,5 +9,30 @@ int main () {
         count--;
     }
 
+// game example
+     int lives = 3;  // Starting number of lives
+    bool gameOver = false;
+
+    while (!gameOver) {
+        cout << "You have " << lives << " lives remaining.\n";
+
+        // Simulate losing a life (you can replace this with actual game logic)
+        cout << "Do you want to continue? (y/n): ";
+        char input;
+        cin >> input;
+
+        if (input == 'y' || input == 'Y') {
+            --lives;  // Decrease the number of lives
+
+            if (lives <= 0) {
+                gameOver = true;  // End the game if no lives are left
+            }
+        } else {
+            cout << "Continuing game...\n";
+        }
+    }
+
+    cout << "Game Over! You have no lives left.\n";
+
     return 0;
 }
